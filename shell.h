b2ec4ee3f99f0c29a527b9cb7f2a8e26aebd1ad1
uoch <chaouch.thameurtc61@gmail.com>
2022-12-02 10:06:45 +0100

    test

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+#define PRINTER(c) (write(STDOUT_FILENO, c, strlen(c)));
+char **split(char *raw_cmd, char *limit);
+#endif
---
+++ split.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+ * split - function taht split the input in an array of string
+ * @raw_cmd: the input
+ * @limit: the delimenter
+ * Return: array of string
+ */
+char **split(char *raw_cmd, char *limit)
+{
+	char	*ptr = NULL;
+	char	**cmd = NULL;
+	size_t	idx = 0;
+
+	ptr = strtok(raw_cmd, limit);
+
+	while (ptr)
+	{
+		cmd = realloc(cmd, ((idx + 1) * sizeof(char *)));
+		cmd[idx] = strdup(ptr);
+		ptr = strtok(NULL, limit);
+		++idx;
+	}
+	cmd = realloc(cmd, ((idx + 1) * sizeof(char *)));
+	cmd[idx] = NULL;
+	return (cmd);
+}
